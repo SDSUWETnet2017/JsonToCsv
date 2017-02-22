3faf3fd02d1d4cf71b9b7451f3d418551b923bbe
Yusufsh27 <yusufsh23@gmail.com>
2017-02-22 08:42:44 -0800

    Adding Feature to make unique

--- CompE490/JsonToCsv.py
+++ CompE490/JsonToCsv.py
@@ -1,18 +1,49 @@
 import json
 import csv
+import ijson
+from traitlets.config.loader import JSONFileConfigLoader
+import os
+from os import path
+
+cwd = os.getcwd()
+print(cwd)
+
+#path = '..\\test_data.json'
+path = '..\\md_traffic.json'
 
 
-employee_data = '{"employee_details":[{"employee_name": "James", "email": "james@gmail.com", "job_profile": "Sr. Developer"},{"employee_name": "Smith", "email": "Smith@gmail.com", "job_profile": "Project Lead"}]}'
+
+#edit
+
+with open(path,'r') as jsonfile:
+    objects = ijson.items(jsonfile, 'meta.view.columns.item')
+    #objects = ijson.items(jsonfile, 'node1.lists.item')
+    print(objects)
+    columns = list(objects)
+    
+print(columns)
+
+CSVfile = open('/EmployData.csv', 'w')
+csvwriter = csv.writer(CSVfile)
+print(csvwriter)
+
+count = 0
+for emp in columns:
+    if count == 0:
+        header = emp.keys()
+        csvwriter.writerow(header)
+        count += 1
+    csvwriter.writerow(emp.values())
 
 
-X = json.loads(employee_data) # This loads the json file into a variable X
-CSVfile = open('./EmployData.csv', 'w') # This opens a new CSV file with write permissions
 
 
-for key in X.keys():
+"""for key in X.keys():
     firstProp = key #Gets the Key value inside employee data
     
-    
+for item in ijson.items(f, 'meta.view.columns.item'):
+        print(item)
+        break
     
 emp_data = X[key] #Prints the data associated with the key
 csvwriter = csv.writer(CSVfile)
@@ -26,4 +57,4 @@ for emp in emp_data:
         count += 1
     csvwriter.writerow(emp.values())
 
-CSVfile.close()
+CSVfile.close()"""
